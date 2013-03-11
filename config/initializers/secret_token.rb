08bf4f70d24d02428232ea0670c3eaf6dc26dc6a
Jonathan Arp <jarp@nd.edu>
2013-03-11 15:21:48 -0400

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamRosterApp::Application.config.secret_token = '183fa156aa1a2c014d7e376be6fe5ea90a245611ff9fa811f6925a2a7fc4741c118b7441323e7de7be27595c4356d4722dbf8f4419daf4cdcd7001000c233e18'
